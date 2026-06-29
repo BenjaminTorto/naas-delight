b7104b9483dfc5dcd63201592978f86574b1f5d9
Benjamin Torto <benjamintorto4@gmail.com>
2026-06-29 18:43:04 +0000

    Update typography to Playfair Display + Montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ export default {
         muted: '#8A7E6A',
       },
       fontFamily: {
-        serif: ['Cormorant Garamond', 'Georgia', 'serif'],
-        sans: ['Outfit', 'sans-serif'],
+  serif: ['Playfair Display', 'Georgia', 'serif'],
+  sans: ['Montserrat', 'sans-serif'],
       },
       keyframes: {
         ticker: {
